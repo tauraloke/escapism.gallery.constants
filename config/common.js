4977001b150fd4fb9422c2f1f8544ec167eafb6f
tauraloke <tauraloke@gmail.com>
2021-09-24 10:54:02 +0300

    images

--- config/common.js
+++ config/common.js
@@ -10,5 +10,6 @@ module.exports = {
   USERPAGE_HEADER_WIDTH: 2000,
   USERPAGE_HEADER_HEIGHT: 220,
   TEMP_IMAGE_DAY_EXPIRED: 30,
-  SPOILED_IMAGE_PATH: "/spoiled.webp",
+  SPOILED_IMAGE_PATH: "/resources/expired_image.webp",
+  UNKNOWN_AVATAR_PATH: "/resources/unknown_avatar.webp",
 };
